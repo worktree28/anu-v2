6efa2d45aa094503297a79666e6217ee7f24369c
worktree28 <60058458+worktree28@users.noreply.github.com>
2022-06-18 16:19:29 +0000

    anu

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
           speed={0.05}
           onClick={() => ref.current.scrollTo(3)}
         >
-          <h2>Welcome to my website</h2>
+          <h2>Hipity Hopity</h2>
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -59,7 +59,7 @@ function App() {
           speed={2}
           onClick={() => ref.current.scrollTo(0)}
         >
-          <h2>Hi Mom!</h2>
+          <h2>Anu landed onto my property</h2>
         </ParallaxLayer>
       </Parallax>
     </div>
